a4a7631f25c836f445ec6e5f3cacdfd68a43ebcc
Steve Birtles <sbirtles@farnborough.ac.uk>
2018-10-31 21:53:25 +0000

    Nearly ready for Gophers.

--- shaders.go
+++ shaders.go
@@ -66,29 +66,29 @@ func initiateShaders() {
 func finaliseShaders() {
 
 	projection := mgl32.Perspective(mgl32.DegToRad(45.0), float32(windowWidth)/windowHeight, 0.1, 5000.0)
-	projectionUniform := gl.GetUniformLocation(program, gl.Str("projection\x00"))
+	projectionUniform := gl.GetUniformLocation(program, gl.Str("projection"+"\x00"))
 	gl.UniformMatrix4fv(projectionUniform, 1, false, &projection[0])
 
 	model = mgl32.Ident4()
-	modelUniform = gl.GetUniformLocation(program, gl.Str("model\x00"))
+	modelUniform = gl.GetUniformLocation(program, gl.Str("model"+"\x00"))
 	gl.UniformMatrix4fv(modelUniform, 1, false, &model[0])
 
-	textureUniform := gl.GetUniformLocation(program, gl.Str("tex\x00"))
+	textureUniform := gl.GetUniformLocation(program, gl.Str("tex"+"\x00"))
 	gl.Uniform1i(textureUniform, 0)
 
-	gl.BindFragDataLocation(program, 0, gl.Str("outputColor\x00"))
-
-	vertAttrib := uint32(gl.GetAttribLocation(program, gl.Str("vert\x00")))
+	vertAttrib := uint32(gl.GetAttribLocation(program, gl.Str("vert"+"\x00")))
 	gl.EnableVertexAttribArray(vertAttrib)
 	gl.VertexAttribPointer(vertAttrib, 3, gl.FLOAT, false, 8*4, gl.PtrOffset(0))
 
-	texCoordAttrib := uint32(gl.GetAttribLocation(program, gl.Str("vertTexCoord\x00")))
+	texCoordAttrib := uint32(gl.GetAttribLocation(program, gl.Str("vertTexCoord"+"\x00")))
 	gl.EnableVertexAttribArray(texCoordAttrib)
 	gl.VertexAttribPointer(texCoordAttrib, 2, gl.FLOAT, false, 8*4, gl.PtrOffset(3*4))
 
-	colorAttrib := uint32(gl.GetAttribLocation(program, gl.Str("inputColor\x00")))
+	colorAttrib := uint32(gl.GetAttribLocation(program, gl.Str("inputColor"+"\x00")))
 	gl.EnableVertexAttribArray(colorAttrib)
 	gl.VertexAttribPointer(colorAttrib, 3, gl.FLOAT, false, 8*4, gl.PtrOffset(5*4))
+
+	gl.BindFragDataLocation(program, 0, gl.Str("outputColor"+"\x00"))
 
 }
 
